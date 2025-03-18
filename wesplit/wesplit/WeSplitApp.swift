e717ebb00894c427b9bf7cdeeb148906efed335e
Brendan Whittemore <brendan.whittemore@icloud.com>
2025-03-18 18:44:28 -0400

    create xcode project

---
+++ wesplit/wesplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Brendan Whittemore on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
