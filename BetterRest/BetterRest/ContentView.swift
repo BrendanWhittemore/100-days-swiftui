29a151c6886eaf3bc974b1b1970b14ae09ab4772
Brendan Whittemore <brendan.whittemore@icloud.com>
2025-04-07 16:19:40 -0400

    create xcode project and machine learning model

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Brendan Whittemore on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
