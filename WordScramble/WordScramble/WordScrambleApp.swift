195070dd927379be2328b7495932346a6ee54bef
Brendan Whittemore <brendan.whittemore@icloud.com>
2025-04-13 20:12:16 -0400

    create project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Brendan Whittemore on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
